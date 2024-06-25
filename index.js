8edb9890dc3ab2fb41ee6a274210b62e8dd9ffd9
Rounak Chatterjee <123956143+RounakChatterjee2004@users.noreply.github.com>
2024-06-25 21:03:10 +0530

    Add files via upload
    backend 75% done

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+async function main() {
+  try {
+    console.log("Connecting to MongoDB with URI:", process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {});
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Could not connect to MongoDB", err);
+  }
+}
+
+main();
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.get("/", (req, res) => {
+  res.send("WELCOME TO HOMEPAGE");
+});
+
+const PORT = 8800;
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
