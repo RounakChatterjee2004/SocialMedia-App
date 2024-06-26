6135dd0394522f243fc9f1350264f6b573cf9193
Rounak Chatterjee <123956143+RounakChatterjee2004@users.noreply.github.com>
2024-06-27 01:02:16 +0530

    Add nodeRestApi folder

---
+++ nodeRestApi/routes/posts.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Posts"); // Adjust the path as needed
+
+// Create post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated");
+    } else {
+      res.status(403).json("You can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Like & dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // The user will like the post if they have not liked it till now
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    }
+
+    // The user has already liked the post and wants to unlike
+    else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
+//get a post
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get timeline posts all posts
+router.get("/timeline", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+
+    const userPosts = await Post.find({ userId: currentUser._id });
+
+    //Promise.all is used to handle multiple asynchronous operations in parallel, ensuring that all friend posts are fetched before proceeding.
+
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
